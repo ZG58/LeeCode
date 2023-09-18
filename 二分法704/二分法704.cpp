10961679c13c550bb4f449d9421894a7cfc33e36
ZG58 <76912129+ZG58@users.noreply.github.com>
2023-09-18 22:58:50 +0800

    添加项目文件。

---
+++ 二分法704/二分法704.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int middle = left + ((right - left) / 2);
+            if (nums[middle] > target) {
+                right = middle - 1;
+            }
+            else if (nums[middle] < target) {
+                left = middle + 1;
+            }
+            else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+    vector<int> nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int target = 5;
+    Solution solution;
+    int result = solution.search(nums, target);
+
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
---
+++ 移除元素27/移除元素27.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.size(); fastIndex++) {
+            if (nums[fastIndex] != val) {
+                nums[slowIndex++] = nums[fastIndex];
+            }
+        }
+        return slowIndex;
+    }
+};
+
+int main() {
+    vector<int> nums = { 1, 2, 3, 3, 4, 3, 6 };
+    int target = 3;
+    Solution sloution;
+    int result = sloution.removeElement(nums, target);
+
+    cout << "New Length: " << result << endl;
+
+    cout << "Modified Vector: ";
+    for (int i = 0; i < result; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
