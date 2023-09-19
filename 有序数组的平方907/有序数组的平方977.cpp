aa3c20fdb468733d795698889a60b09ce8374f41
ZG58 <76912129+ZG58@users.noreply.github.com>
2023-09-19 20:58:58 +0800

    有序数组的平方

---
+++ 有序数组的平方907/有序数组的平方977.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> sortedSquares(vector<int>& nums) {
+		int k = nums.size() - 1;
+		vector<int> result(nums.size(), 0);
+		for (int i = 0, j = nums.size() - 1; i <= j;) {
+			if (nums[i] * nums[i] < nums[j] * nums[j]) {
+				result[k--] = nums[j] * nums[j];
+				j--;
+			}
+			else {
+				result[k--] = nums[i] * nums[i];
+				i++;
+			}
+		}
+		return result;
+	}
+};
+
+int main() {
+	vector<int> nums1 = { 1, 2, 3, 3, 4, 3, 6 };
+	vector<int> nums2 = { 7, 8, 9, 2, 3, 3 };
+	Solution solution;
+	vector<int> result1 = solution.sortedSquares(nums1);
+	vector<int> result2 = solution.sortedSquares(nums2);
+
+	cout << "result1 : ";
+	for (int i = 0; i < result1.size(); i++) {
+		cout << result1[i] << " ";
+	}
+	cout << endl;
+	cout << "result2 :";
+	for (int i = 0; i < result2.size(); i++) {
+		cout << result2[i] << " ";
+	}
+	cout << endl;
+}
