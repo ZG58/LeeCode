9ed917d05883d39e3d6dbf5621279dce5e6c5700
ZG58 <76912129+ZG58@users.noreply.github.com>
2023-09-22 23:53:10 +0800

    203

---
+++ 移除链表元素203/移除链表元素203.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+	ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* cur = dummyHead;
+        while (cur->next != nullptr) {
+            if (cur->next->val == val) {
+                ListNode* temp = cur->next;
+                cur->next = cur->next->next;
+                delete temp;
+            }
+            else {
+                cur = cur->next;
+            }
+        }
+        head = dummyHead->next;
+        delete dummyHead;
+        return head;
+	}
+};
+
+void printList(ListNode* head) {
+    ListNode* current = head;
+    while (current != nullptr) {
+        std::cout << current->val << " ";
+        current = current->next;
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    // 创建一个测试链表：1 -> 2 -> 6 -> 3 -> 4 -> 5 -> 6
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(6);
+    head->next->next->next = new ListNode(3);
+    head->next->next->next->next = new ListNode(4);
+    head->next->next->next->next->next = new ListNode(5);
+    head->next->next->next->next->next->next = new ListNode(6);
+
+    std::cout << "Original List: ";
+    printList(head);
+
+    Solution solution;
+    int valToRemove = 3;
+    ListNode* newHead = solution.removeElements(head, valToRemove);
+
+    std::cout << "List after removing " << valToRemove << ": ";
+    printList(newHead);
+
+    while (newHead != nullptr) {
+        ListNode* temp = newHead;
+        newHead = newHead->next;
+        delete temp;
+    }
+
+    return 0;
+}
