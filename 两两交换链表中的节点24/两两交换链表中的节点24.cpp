d7e3d6a27428d2871b7af6a311b11fe460de7668
ZG58 <76912129+ZG58@users.noreply.github.com>
2023-09-23 22:12:29 +0800

    24递归

---
+++ 两两交换链表中的节点24/两两交换链表中的节点24.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) return nullptr;
+        if (head->next == nullptr) return head;
+        ListNode* last = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return last;
+    }
+
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr) return nullptr;
+        if (head->next == nullptr) return head;
+        ListNode* last = swapPairs(head->next->next);
+        ListNode* temp = head->next;
+        head->next->next = head;
+        head->next = last;
+        return temp;
+    }
+};
+
+void printList(ListNode* head) {
+    ListNode* current = head;
+    while (current != nullptr) {
+        std::cout << current->val << " ";
+        current = current->next;
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    // 创建一个测试链表：1 -> 2 -> 6 -> 3 -> 4 -> 5 -> 6
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(6);
+    head->next->next->next = new ListNode(3);
+    head->next->next->next->next = new ListNode(4);
+    head->next->next->next->next->next = new ListNode(5);
+    head->next->next->next->next->next->next = new ListNode(6);
+
+    std::cout << "Original List: ";
+    printList(head);
+
+    Solution solution;
+    ListNode* newHead = solution.swapPairs(head);
+
+    std::cout << "List after rever: ";
+    printList(newHead);
+
+    while (newHead != nullptr) {
+        ListNode* temp = newHead;
+        newHead = newHead->next;
+        delete temp;
+    }
+
+    return 0;
+}
