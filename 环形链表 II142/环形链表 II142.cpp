8180164a08366f6f909fafce9e3146c37ab02b2f
ZG58 <76912129+ZG58@users.noreply.github.com>
2023-09-27 23:05:10 +0800

    142

---
+++ 环形链表 II142/环形链表 II142.cpp
@@ -0,0 +1,153 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* cur = dummyHead;
+        while (cur->next != nullptr) {
+            if (cur->next->val == val) {
+                ListNode* temp = cur->next;
+                cur->next = cur->next->next;
+                delete temp;
+            }
+            else {
+                cur = cur->next;
+            }
+        }
+        head = dummyHead->next;
+        delete dummyHead;
+        return head;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) return nullptr;
+        if (head->next == nullptr) return head;
+        ListNode* last = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return last;
+    }
+
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr) return nullptr;
+        if (head->next == nullptr) return head;
+        ListNode* last = swapPairs(head->next->next);
+        ListNode* temp = head->next;
+        head->next->next = head;
+        head->next = last;
+        return temp;
+    }
+
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* fast = dummyHead;
+        ListNode* slow = dummyHead;
+        while (n-- && fast != nullptr) {
+            fast = fast->next;
+        }
+        while (fast->next != nullptr) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        ListNode* temp = slow->next;
+        slow->next = slow->next->next;
+        delete temp;
+        return dummyHead->next;
+    }
+
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        ListNode* curA = headA;
+        ListNode* curB = headB;
+        int lenA = 0, lenB = 0;
+        while (curA != nullptr) {
+            lenA++;
+            curA = curA->next;
+        }
+        while (curB != nullptr) {
+            lenB++;
+            curB = curB->next;
+        }
+        curA = headA;
+        curB = headB;
+        if (lenB > lenA) {
+            swap(lenA, lenB);
+            swap(curA, curB);
+        }
+        int gap = lenA - lenB;
+        while (gap--) {
+            curA = curA->next;
+        }
+        while (curA != nullptr) {
+            if (curA == curB) return curA;
+            curA = curA->next;
+            curB = curB->next;
+        }
+        return nullptr;
+    }
+
+    ListNode* detectCycle(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) {
+                ListNode* index1 = head;
+                ListNode* index2 = fast;
+                while (index1 != index2) {
+                    index1 = index1->next;
+                    index2 = index2->next;
+                }
+                return index1;
+            }
+        }
+        return nullptr;
+    }
+};
+
+void printList(ListNode* head) {
+    ListNode* current = head;
+    while (current != nullptr) {
+        std::cout << current->val << " ";
+        current = current->next;
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(6);
+    head->next->next->next = new ListNode(3);
+    head->next->next->next->next = new ListNode(4);
+    head->next->next->next->next->next = new ListNode(5);
+    head->next->next->next->next->next->next = new ListNode(6);
+    head->next->next->next->next->next->next->next = head->next->next->next;
+
+    Solution solution;
+
+    ListNode* cycleStart = solution.detectCycle(head);
+
+    if (cycleStart != nullptr) {
+        std::cout << "环的起始节点值为: " << cycleStart->val << std::endl;
+    }
+    else {
+        std::cout << "没有环" << std::endl;
+    }
+
+    return 0;
+}
