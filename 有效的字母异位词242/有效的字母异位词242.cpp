6f5930c5715bda95c20d4bf84f14d25dad332748
ZG58 <76912129+ZG58@users.noreply.github.com>
2023-09-28 23:58:51 +0800

    242

---
+++ 有效的字母异位词242/有效的字母异位词242.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
+
+int main() {
+    Solution solution;
+
+    assert(solution.isAnagram("anagram", "nagaram") == true);
+    assert(solution.isAnagram("rat", "car") == false);
+    assert(solution.isAnagram("listen", "silent") == true);
+    assert(solution.isAnagram("hello", "world") == false);
+
+    cout << "All test cases passed!" << endl;
+
+    return 0;
+}
