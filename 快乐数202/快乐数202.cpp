aaf92924f11a264165da38b8d50ce52319304a5b
ZG58 <76912129+ZG58@users.noreply.github.com>
2023-09-30 22:33:07 +0800

    202

---
+++ 快乐数202/快乐数202.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+	int getSum(int n) {
+		int sum = 0;
+		while (n) {
+			sum += (n % 10) * (n % 10);
+			n /= 10;
+		}
+		return sum;
+	}
+	bool isHappy(int n) {
+		unordered_set<int> set;
+		while (1) {
+			int sum = getSum(n);
+			if (sum == 1) {
+				return true;
+			}
+			if (set.find(sum) != set.end()) {
+				return false;
+			}
+			else {
+				set.insert(sum);
+			}
+			n = sum;
+		}
+	}
+};
+
+int main()
+{
+	int n = 2;
+	Solution solution;
+	
+	if (solution.isHappy(n)) {
+		cout << n << " is a happy number" << endl;
+	}
+	else {
+		cout << n << " is not a happy number" << endl;
+	}
+
+	return 0;
+}
