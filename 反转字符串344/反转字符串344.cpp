50e5b644801e9e26b9acbfc68b7040da260dc3b6
ZG58 <76912129+ZG58@users.noreply.github.com>
2023-12-07 14:39:15 +0800

    反转字符串344

---
+++ 反转字符串344/反转字符串344.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	void reverseString(vector<char>& s) {
+		for (int i = 0, j = s.size() - 1; i < s.size() / 2; i++, j--) {
+			swap(s[i], s[j]);
+		}
+	}
+};
+
+int main()
+{
+	Solution solution;
+
+    // 声明一个空的字符向量
+    std::vector<char> str1;
+
+    std::cout << "请输入字符串（按Enter键结束输入）: ";
+
+    // 使用cin读取用户输入的字符，直到用户按Enter键
+    char ch;
+    while (std::cin.get(ch) && ch != '\n') {
+        // 将字符添加到向量中
+        str1.push_back(ch);
+    }
+
+    solution.reverseString(str1);
+
+    // 输出应为 "olleh"
+    for (char c : str1) {
+        cout << c;
+    }
+    cout << endl;
+
+	return 0;
+}
+
+
