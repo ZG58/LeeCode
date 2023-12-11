6c79b294d10a8e10aecaa73191295471564d1ac6
ZG58 <76912129+ZG58@users.noreply.github.com>
2023-12-11 12:54:41 +0800

    分发饼干455

---
+++ 分发饼干455/分发饼干455.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int findContentChildren(vector<int>& g, vector<int>& s) {
+		sort(g.begin(), g.end());
+		sort(s.begin(), s.end());
+
+		int index = 0;
+		for (int i = 0; i < s.size(); i++) {
+			if (index < g.size() && g[index] <= s[i]) {
+				index++;
+			}
+		}
+		return index;
+	}
+};
+int main() {
+	Solution solution;
+	vector<int> g = { 1, 2, 3 };
+	vector<int> s = { 1, 1 };
+
+	int result = solution.findContentChildren(g, s);
+
+	// 期望输出：1，因为只有第一个孩子的胃口可以被满足
+	cout << "满足孩子的数量为：" << result << endl;
+
+	return 0;
+}
