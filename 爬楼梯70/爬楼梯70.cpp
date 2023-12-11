a3532e06d6a577b57c992cdebeb2fac4e95d20d1
ZG58 <76912129+ZG58@users.noreply.github.com>
2023-12-12 01:42:45 +0800

    递归

--- 爬楼梯70/爬楼梯70.cpp
+++ 爬楼梯70/爬楼梯70.cpp
@@ -3,17 +3,25 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//	int climbStairs(int n) {
+//		if (n <= 1) return n;
+//		vector<int> dp(n + 1);
+//		dp[1] = 1;
+//		dp[2] = 2;
+//		for (int i = 3; i <= n; i++) {
+//			dp[i] = dp[i - 1] + dp[i - 2];
+//		}
+//		return dp[n];
+//	}
+//};
+
 class Solution {
 public:
 	int climbStairs(int n) {
-		if (n <= 1) return n;
-		vector<int> dp(n + 1);
-		dp[1] = 1;
-		dp[2] = 2;
-		for (int i = 3; i <= n; i++) {
-			dp[i] = dp[i - 1] + dp[i - 2];
-		}
-		return dp[n];
+		if (n <= 2) return n;
+		return climbStairs(n - 1) + climbStairs(n - 2);
 	}
 };
 
