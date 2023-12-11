014bf97996890912eb586a183b11dc72a9c2d220
ZG58 <76912129+ZG58@users.noreply.github.com>
2023-12-12 02:10:55 +0800

    节省空间

--- 使用最小花费爬楼梯746/使用最小花费爬楼梯746.cpp
+++ 使用最小花费爬楼梯746/使用最小花费爬楼梯746.cpp
@@ -3,16 +3,30 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//	int minCostClimbingStairs(vector<int>& cost) {
+//		vector<int> dp(cost.size() + 1);
+//		dp[0] = 0;
+//		dp[1] = 0;
+//		for (int i = 2; i <= cost.size(); i++) {
+//			dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+//		}
+//		return dp[cost.size()];
+//	}
+//};
+
 class Solution {
 public:
 	int minCostClimbingStairs(vector<int>& cost) {
-		vector<int> dp(cost.size() + 1);
-		dp[0] = 0;
-		dp[1] = 0;
+		int dp0 = 0;
+		int dp1 = 0;
 		for (int i = 2; i <= cost.size(); i++) {
-			dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+			int dpi = min(dp1 + cost[i - 1], dp0 + cost[i - 2]);
+			dp0 = dp1;
+			dp1 = dpi;
 		}
-		return dp[cost.size()];
+		return dp1;
 	}
 };
 
