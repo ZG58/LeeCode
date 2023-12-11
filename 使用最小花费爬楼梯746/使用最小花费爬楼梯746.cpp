f4fa82498380a30195fed37968178024a59ecad5
ZG58 <76912129+ZG58@users.noreply.github.com>
2023-12-12 02:01:32 +0800

    746

---
+++ 使用最小花费爬楼梯746/使用最小花费爬楼梯746.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int minCostClimbingStairs(vector<int>& cost) {
+		vector<int> dp(cost.size() + 1);
+		dp[0] = 0;
+		dp[1] = 0;
+		for (int i = 2; i <= cost.size(); i++) {
+			dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+		}
+		return dp[cost.size()];
+	}
+};
+
+int main()
+{
+	Solution solution;
+	vector<int> cost = { 10, 15, 20 };
+
+	int result = solution.minCostClimbingStairs(cost);
+
+	cout << "爬到楼顶的最小花费是:" << result << endl;
+
+	return 0;
+}
