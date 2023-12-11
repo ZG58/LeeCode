86b03d631036f13f1146399065bb0330b701a0fd
ZG58 <76912129+ZG58@users.noreply.github.com>
2023-12-12 01:20:28 +0800

    509

---
+++ 斐波那契数509/斐波那契数509.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int fib(int n) {
+		if (n <= 1) return n;
+		int dp[2];
+		dp[0] = 0;
+		dp[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			int sum = dp[0] + dp[1];
+			dp[0] = dp[1];
+			dp[1] = sum;
+		}
+		return dp[1];
+	}
+};
+
+// 测试代码
+int main() {
+	Solution solution;
+
+	// 测试1
+	int result1 = solution.fib(5);
+	// 期望输出：5，对应斐波那契数列的第5个数为 5
+	cout << "第5个斐波那契数为：" << result1 << endl;
+
+	// 测试2
+	int result2 = solution.fib(10);
+	// 期望输出：55，对应斐波那契数列的第10个数为 55
+	cout << "第10个斐波那契数为：" << result2 << endl;
+
+	return 0;
+}
