9f9dec4d7b20167f1ad321ac94d2e7bce265ce83
ZG58 <76912129+ZG58@users.noreply.github.com>
2023-12-12 01:25:49 +0800

    递归

--- 斐波那契数509/斐波那契数509.cpp
+++ 斐波那契数509/斐波那契数509.cpp
@@ -3,19 +3,27 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//	int fib(int n) {
+//		if (n <= 1) return n;
+//		int dp[2];
+//		dp[0] = 0;
+//		dp[1] = 1;
+//		for (int i = 2; i <= n; i++) {
+//			int sum = dp[0] + dp[1];
+//			dp[0] = dp[1];
+//			dp[1] = sum;
+//		}
+//		return dp[1];
+//	}
+//};
+
 class Solution {
 public:
 	int fib(int n) {
-		if (n <= 1) return n;
-		int dp[2];
-		dp[0] = 0;
-		dp[1] = 1;
-		for (int i = 2; i <= n; i++) {
-			int sum = dp[0] + dp[1];
-			dp[0] = dp[1];
-			dp[1] = sum;
-		}
-		return dp[1];
+		if (n < 2) return n;
+		return fib(n - 1) + fib(n - 2);
 	}
 };
 
